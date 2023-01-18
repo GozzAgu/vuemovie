a24dcdf5b085de15dcb07c9d231b18624ea02b12
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-01-18 17:07:36 +0100

    feat: added a search bar for movies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
+    <form @submit.prevent="searchMovies()" class="search">
+      <input type="text" v-model="search" placeholder="What are you looking for?" />
+      <input type="submit" value="Search" />
+    </form>
+
     <div class="feature-card">
-      <router-link to="/movie/tt0409591">
+      <router-link to="/movie/Naruto">
         <img src="" />
         <div class="detail">
           <h3>Naruto</h3>
         </div>
       </router-link>
     </div>
+
+    <div class="movies-list">
+      <h1>Movies</h1>
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
 
+const search = ref('');
+
+const searchMovies = () => {
+  if(search.value !== '') {
+    console.log(search.value)
+  }
+}
 </script>
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="movie-detail">
+    <h1>{{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
