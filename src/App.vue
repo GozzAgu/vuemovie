87d00f6cdf432779bfc55c09c5064609415c95b8
GozAgu <103033742+GozzAgu@users.noreply.github.com>
2023-01-18 16:33:13 +0100

    feat: Added router to home

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
   </nav>
-  <router-view/>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
 }
 
 nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 15px;
 }
+
+span {
+  color: blueviolet;
+}
+
 </style>
