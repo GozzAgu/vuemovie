ac39366cc587b21fd250bee476bd2591d8a2b77c
GozzAgu <103033742+GozzAgu@users.noreply.github.com>
2023-01-20 21:38:16 +0100

    feta: added route param for movie

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="movie-detail">
-    <h1>{{ $route.params.id }}</h1>
+    <h2>{{ movie.Title }}</h2>
+    <img :src="movie.Poster" class="card-img-top" alt="...">
+    <h4>{{ movie.Actors }}</h4>
+    <h4>{{ movie.Plot }}</h4>
   </div>
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from './env';
 
+const movie = ref({});
+const route = useRoute();
+
+onBeforeMount(() => {
+  fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+  .then(response => response.json())
+  .then(data => {
+    movie.value = data;
+    console.log(data)
+  })
+})
 </script>
 
 <style>
